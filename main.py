9bf8bda566e083ebe1dd52eb6ba0f3d78ca2ca7c
AinTEAsports <benkirat.khalil@gmail.com>
2022-05-15 15:29:04 +0200

    main.py : added help custom command

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands, tasks
 from profileCog import ProfileEmbed
 
 
-bot = commands.Bot(command_prefix=".sp ", description="A simple bo to test a cog I'm making")
+bot = commands.Bot(command_prefix=".sp ", description="A simple bo to test a cog I'm making", help_command=None)
 
 
 
@@ -15,6 +15,37 @@ bot = commands.Bot(command_prefix=".sp ", description="A simple bo to test a cog
 async def on_ready():
     print(f"Ready, logged in as {bot.user}")
 
+
+
+################################# HELP COMMAND #######################################
+
+
+@bot.command()
+async def help(ctx):
+    commands = {
+        "tag" : "to display/change your current tag",
+        "thumbnail" : "to display change your current thumbnail image",
+        "mains" : "to display/change your current mains",
+        "secondaries" : "to display/change your current secondaries",
+        "games" : "to display/change your current games",
+        "switch_code" : "to display/change your current switch code",
+        "region" : "to display/change your current region",
+        "note" : "to display/change your current note",
+        "reset_profile" : "to reset your profile",
+        "profile" : "to show your profile in a beautiful embed",
+    }
+
+    embedString = ""
+
+    for command, description in commands.items():
+        embedString += f"**{command}** {description}\n"
+        
+        
+    helpEmbed = discord.Embed(title="__Help embed__")
+    helpEmbed.add_field(name="**Commands :**\n", value=embedString, inline=False)
+    helpEmbed.set_footer(icon_url=ctx.message.guild.icon_url, text=ctx.message.guild.name)
+    
+    await ctx.send(embed=helpEmbed)
 
 
 ################################# COG ADDS #######################################
